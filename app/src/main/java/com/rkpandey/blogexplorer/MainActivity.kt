4d2366bd74dbe3c8cedfd414b024e17cf93be8e9
Rahul Pandey <rpandey1234@gmail.com>
2021-06-02 13:02:34 -0700

    Starting point - added layout for main acivity + viewbinding

--- app/src/main/java/com/rkpandey/blogexplorer/MainActivity.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/MainActivity.kt
@@ -2,10 +2,22 @@ package com.rkpandey.blogexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.rkpandey.blogexplorer.databinding.ActivityMainBinding
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.button.setOnClickListener {
+            viewModel.getPosts()
+        }
     }
 }
---
+++ app/src/main/java/com/rkpandey/blogexplorer/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.rkpandey.blogexplorer
+
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "MainViewModel"
+class MainViewModel : ViewModel() {
+
+    fun getPosts() {
+        // TODO fill this in
+    }
+}
