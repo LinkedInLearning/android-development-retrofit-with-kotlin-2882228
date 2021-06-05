d43514a27ee12ca63810ab112e6ffac5d064ad0c
Rahul Pandey <rpandey1234@gmail.com>
2021-06-04 17:50:22 -0700

    Ability to make PUT request

--- app/src/main/java/com/rkpandey/blogexplorer/edit/EditViewModel.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/edit/EditViewModel.kt
@@ -1,9 +1,13 @@
 package com.rkpandey.blogexplorer.edit
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rkpandey.blogexplorer.api.RetrofitInstance
 import com.rkpandey.blogexplorer.models.Post
+import kotlinx.coroutines.launch
 
 private const val TAG = "EditViewModel"
 class EditViewModel : ViewModel() {
@@ -16,7 +20,12 @@ class EditViewModel : ViewModel() {
         get() = _currentStatus
 
     fun updatePost(postId: Int, newPostData: Post) {
-        // TODO: send PUT request
+        viewModelScope.launch {
+            _post.value = null
+            val updatedPost = RetrofitInstance.api.updatePost(postId, newPostData)
+            Log.i(TAG, "updated post $updatedPost")
+            _post.value = updatedPost
+        }
     }
 
     fun patchPost(postId: Int, title: String, body: String) {
--- app/src/main/java/com/rkpandey/blogexplorer/models/Post.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/models/Post.kt
@@ -1,7 +1,9 @@
 package com.rkpandey.blogexplorer.models
 
+import java.io.Serializable
+
 data class Post(
     val userId: Int,
     val id: Int,
     val title: String,
-    val body: String)
+    val body: String): Serializable
