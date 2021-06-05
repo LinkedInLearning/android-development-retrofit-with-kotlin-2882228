af25de594443242a03f5419ff2fc9d5c1b11781c
Rahul Pandey <rpandey1234@gmail.com>
2021-06-04 17:30:04 -0700

    Error handling in MainActivity

--- app/src/main/java/com/rkpandey/blogexplorer/MainViewModel.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/MainViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.rkpandey.blogexplorer.api.RetrofitInstance
 import com.rkpandey.blogexplorer.models.Post
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 private const val TAG = "MainViewModel"
 class MainViewModel : ViewModel() {
@@ -19,13 +20,24 @@ class MainViewModel : ViewModel() {
     val isLoading: LiveData<Boolean>
         get() = _isLoading
 
+    private val _errorMessage = MutableLiveData<String?>(null)
+    val errorMessage: LiveData<String?>
+        get() = _errorMessage
+
     fun getPosts() {
         viewModelScope.launch {
+            _errorMessage.value = null
             _isLoading.value = true
-            val fetchedPosts = RetrofitInstance.api.getPosts()
-            Log.i(TAG, "Got posts: $fetchedPosts")
-            _posts.value = fetchedPosts
-            _isLoading.value = false
+            try {
+                val fetchedPosts = RetrofitInstance.api.getPosts()
+                Log.i(TAG, "Got posts: $fetchedPosts")
+                _posts.value = fetchedPosts
+            } catch (e: Exception) {
+                _errorMessage.value = e.message
+                Log.e(TAG, "Exception $e")
+            } finally {
+                _isLoading.value = false
+            }
         }
     }
 }
