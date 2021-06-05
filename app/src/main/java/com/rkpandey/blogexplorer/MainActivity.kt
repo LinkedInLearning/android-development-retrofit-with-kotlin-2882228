28c79f5b30301da40d4c7430984f709f50ab8e82
Rahul Pandey <rpandey1234@gmail.com>
2021-06-04 17:13:34 -0700

    Complete basic recycler view implementation

--- app/src/main/java/com/rkpandey/blogexplorer/MainActivity.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.rkpandey.blogexplorer
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rkpandey.blogexplorer.databinding.ActivityMainBinding
+import com.rkpandey.blogexplorer.models.Post
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+    private lateinit var blogPostAdapter: BlogPostAdapter
+    private val blogPosts = mutableListOf<Post>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.posts.observe(this, Observer { posts ->
+            Log.i(TAG, "Number of posts: ${posts.size}")
+            blogPosts.addAll(posts)
+            blogPostAdapter.notifyDataSetChanged()
+        })
+        viewModel.isLoading.observe(this, Observer { isLoading ->
+            Log.i(TAG, "isLoading $isLoading")
+            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+        })
+
+        blogPostAdapter = BlogPostAdapter(this, blogPosts)
+        binding.rvPosts.adapter = blogPostAdapter
+        binding.rvPosts.layoutManager = LinearLayoutManager(this)
+        binding.button.setOnClickListener {
+            viewModel.getPosts()
+        }
 
         binding.button.setOnClickListener {
             viewModel.getPosts()
--- app/src/main/java/com/rkpandey/blogexplorer/MainViewModel.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/MainViewModel.kt
@@ -1,11 +1,31 @@
 package com.rkpandey.blogexplorer
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rkpandey.blogexplorer.api.RetrofitInstance
+import com.rkpandey.blogexplorer.models.Post
+import kotlinx.coroutines.launch
 
 private const val TAG = "MainViewModel"
 class MainViewModel : ViewModel() {
+    private val _posts: MutableLiveData<List<Post>> = MutableLiveData()
+    val posts: LiveData<List<Post>>
+        get() = _posts
+
+    private val _isLoading = MutableLiveData(false)
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
 
     fun getPosts() {
-        // TODO fill this in
+        viewModelScope.launch {
+            _isLoading.value = true
+            val fetchedPosts = RetrofitInstance.api.getPosts()
+            Log.i(TAG, "Got posts: $fetchedPosts")
+            _posts.value = fetchedPosts
+            _isLoading.value = false
+        }
     }
 }
