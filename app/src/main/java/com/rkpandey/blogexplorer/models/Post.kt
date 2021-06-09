48006bafa5785542d7c4d02ef5a966a2e3921f36
Rahul Pandey <rpandey1234@gmail.com>
2021-06-09 14:06:50 -0700

    Add Keep rules and enable minification

--- app/src/main/java/com/rkpandey/blogexplorer/models/Post.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/models/Post.kt
@@ -1,8 +1,10 @@
 package com.rkpandey.blogexplorer.models
 
+import androidx.annotation.Keep
 import com.squareup.moshi.Json
 import java.io.Serializable
 
+@Keep
 data class Post(
     val userId: Int,
     val id: Int,
