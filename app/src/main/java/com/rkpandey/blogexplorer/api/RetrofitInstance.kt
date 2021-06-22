977faf56e5ba409147faf602e10374c07add137d
Rahul Pandey <rpandey1234@gmail.com>
2021-06-22 04:27:30 -0700

    Show 3 ways of adding hedaers

--- app/src/main/java/com/rkpandey/blogexplorer/api/RetrofitInstance.kt
+++ app/src/main/java/com/rkpandey/blogexplorer/api/RetrofitInstance.kt
@@ -1,16 +1,23 @@
 package com.rkpandey.blogexplorer.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 object RetrofitInstance {
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HeaderInterceptor())
+        .build()
+
     /**
      * Use the Retrofit builder to build a retrofit object using a Moshi converter.
      */
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
